9a291e564b5b114963d13dfbcf0a9891533d5a42
a.farhad <a.farhad@DEV-29>
2023-12-05 14:00:42 +0330

    createMigrate

---
+++ Migrate/Migrate.go
@@ -0,0 +1,13 @@
+//package main
+
+//import (
+//	"fmt"
+//	"log"
+//
+//	"https://github.com/ArminaFarhad/DashboardManagment/tree/main/Initializers"
+//	"https://github.com/ArminaFarhad/DashboardManagment/tree/main/Models"
+///)
+
+//func init() {
+
+//}
